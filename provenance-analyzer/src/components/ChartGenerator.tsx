0f1090b883849de6d94a7fd46f72aa80ad349a97
Dylan Wootton <wootton.dylan@gmail.com>
2020-05-11 23:15:54 -0700

    longer example

--- provenance-analyzer/src/components/ChartGenerator.tsx
+++ provenance-analyzer/src/components/ChartGenerator.tsx
@@ -37,29 +37,30 @@ const ChartGenerator = ({ data }) => {
 
   //d3.select(this.refs.xAxis).call(this.xAxis);
   //d3.select(this.refs.yAxis).call(this.yAxis);
-
+  /*
+  
+  
+  */
+  // Note when rendering other things in svgs, you must only render things that svgs can render- they can't render most react components
   return (
     <svg width={width} height={height}>
-      <SvgIcon>
-        <path
-          d="M256,32C132.281,32,32,132.281,32,256s100.281,224,224,224s224-100.281,224-224S379.719,32,256,32z M256,448
+      <path
+        d="M256,32C132.281,32,32,132.281,32,256s100.281,224,224,224s224-100.281,224-224S379.719,32,256,32z M256,448
 c-105.875,0-192-86.125-192-192S150.125,64,256,64s192,86.125,192,192S361.875,448,256,448z M160,192c0-26.5,14.313-48,32-48
 s32,21.5,32,48c0,26.531-14.313,48-32,48S160,218.531,160,192z M288,192c0-26.5,14.313-48,32-48s32,21.5,32,48
 c0,26.531-14.313,48-32,48S288,218.531,288,192z M384,288c-16.594,56.875-68.75,96-128,96c-59.266,0-111.406-39.125-128-96"
-        />
-      </SvgIcon>
+      />
+
       {bars.map((d, i) => {
         return (
-          <div>
-            <rect
-              key={i}
-              x={d.x}
-              y={d.y}
-              width="10"
-              height={d.height}
-              fill={d.fill}
-            />
-          </div>
+          <rect
+            key={i}
+            x={d.x}
+            y={d.y}
+            width="10"
+            height={d.height}
+            fill={d.fill}
+          />
         );
       })}
     </svg>
