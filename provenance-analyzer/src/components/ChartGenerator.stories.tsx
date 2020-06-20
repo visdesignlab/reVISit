d689936944fbe3f4bc729a3679ef5dd9e1b42963
Dylan Wootton <wootton.dylan@gmail.com>
2020-06-20 09:29:41 -0700

    delete old

--- provenance-analyzer/src/components/ChartGenerator.stories.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { storiesOf } from "@storybook/react";
-import ChartGenerator from "./ChartGenerator";
-storiesOf("ChartGenerator", module).add("small", () => (
-  <ChartGenerator data={[20, 34, 80, 112]} />
-));
--- provenance-analyzer/src/components/Photos.stories.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { storiesOf } from "@storybook/react";
-import Photos from "./Photo";
-storiesOf("Photos", module).add("small", () => (
-  <Photos url={"https://jsonplaceholder.typicode.com/photos?albumId=1"} />
-));
