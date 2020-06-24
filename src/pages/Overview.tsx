6754f10ba0d800b77a3f76d672755e894c819b48
Carolina <carolinanobre@gmail.com>
2020-06-24 12:06:58 -0400

    added first event layer iteration

--- src/pages/Overview.tsx
+++ src/pages/Overview.tsx
@@ -1,7 +1,51 @@
 import React from "react";
+import { Button } from 'antd';
+import { relativeProvenanceData } from "../common/data/provenanceMocks.js";
+import EventLayers from "../components/EventLayers";
+import {
+  EyeInvisibleTwoTone,
+  EyeTwoTone,
+  EllipsisOutlined
+} from '@ant-design/icons';
 
-const Overview = () => {
-  return <div>This is overview</div>;
+
+const Overview = ({ location }) => {
+  let newData = relativeProvenanceData[0].map((dataArr) => {
+    return { provGraph: dataArr };
+  });
+  for (let i = 0; i < 2; i++) {
+    newData = newData.concat(_.cloneDeep(newData));
+  }
+
+  //iterate through and create list of unique events; 
+  let allEvents = [];
+
+  newData.map(d => d.provGraph.nodes.map(n => {
+    allEvents.push(n.event)
+  }))
+
+  allEvents = [... new Set(allEvents)].map(d => {
+    return {
+      'title': d,
+      'key': d,
+      'icon': <EyeTwoTone />,
+      children: ['Alex', 'Lane', 'Jeff', 'Noeska'].map(t => {
+        return {
+          'title': d + '_' + t,
+          'key': d + '_' + t,
+          'icon': <EyeTwoTone />,
+          'children': []
+        }
+      })
+    }
+  });
+
+  // {"title": "0-0", "key": "0-0", "children":
+
+  console.log('allEvents', allEvents)
+  // return <Button type="primary">This is an Ant Button</Button>;
+  return <EventLayers gData={allEvents} />
+
 };
 
 export default Overview;
