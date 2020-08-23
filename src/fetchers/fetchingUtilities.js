019f6d6889685f7a080696b6a2cc9bdb40bb7837
Dylan Wootton <wootton.dylan@gmail.com>
2020-08-22 19:35:37 -0700

    added mock route

--- src/fetchers/fetchingUtilities.js
+++ src/fetchers/fetchingUtilities.js
@@ -1,64 +1,65 @@
 export async function mockAPICall(
-    url,
-    returnData,
-    options = {
-        shouldError: false,
-        timeout: 2000
-    }
+  url,
+  returnData,
+  options = {
+    shouldError: false,
+    timeout: 2000,
+  }
 ) {
-    const {
-        shouldError,
-        timeout
-    } = options;
+  console.log("in mockAPI");
 
-    return await new Promise((resolution, rejection) => {
-        setTimeout(() => {
-            if (shouldError) {
-                rejection({
-                    url,
-                    success: false,
-                    error: `mockAPICall error`,
-                });
-            } else {
-                resolution({
-                    url,
-                    success: true,
-                    data: returnData,
-                });
-            }
-        }, timeout);
-    });
+  const { shouldError, timeout } = options;
+  console.log("in mockAPI");
+  return await new Promise((resolution, rejection) => {
+    console.log("dywootto in promiser");
+    setTimeout(() => {
+      if (shouldError) {
+        rejection({
+          url,
+          success: false,
+          error: `mockAPICall error`,
+        });
+      } else {
+        console.log("dywootto in res", returnData, url);
+        resolution({
+          url,
+          success: true,
+          data: returnData,
+        });
+      }
+    }, timeout);
+  });
 }
 
 export async function fetchData(uri, fetchOptions) {
-    let defaultHeaders = {
-        headers: {
-            Accept: "application/json",
-        },
-    };
-    return await completePromise(uri, defaultHeaders);
+  let defaultHeaders = {
+    headers: {
+      Accept: "application/json",
+    },
+  };
+  return await completePromise(uri, defaultHeaders);
 }
 
 export async function postData(uri, postBody, fetchOptions) {
-    let defaultHeaders = {
-        url: uri,
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json; charset=UTF-8",
-        },
-        body: JSON.stringify(postBody),
-    };
-    return await completePromise(uri, defaultHeaders);
+  let defaultHeaders = {
+    url: uri,
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json; charset=UTF-8",
+    },
+    body: JSON.stringify(postBody),
+  };
+  return await completePromise(uri, defaultHeaders);
 }
 export async function completePromise(uri, requestOptions) {
-    let response;
-    try {
-        response = await fetch(uri, requestOptions);
-        console.log("inpromise", response);
-        response.data = await response.json();
-        response.success = response.ok;
-    } catch (err) {
-        throw err;
-    }
-    return response;
+  let response;
+  try {
+    response = await fetch(uri, requestOptions);
+    console.log("inpromise", response);
+    response.data = await response.json();
+    response.success = response.ok;
+  } catch (err) {
+    throw err;
+  }
+  return response;
 }
